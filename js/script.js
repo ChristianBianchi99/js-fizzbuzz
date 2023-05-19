dbfdff1d3b042c2f8dea594b71ec2c9d16420dca
ChristianBianchi99 <bianchi848@gmail.com>
2023-05-19 16:56:25 +0200

    Agg logica js - esercizio finito

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 "use strict";
+
+// DICHIARAZIONE VARIANTI
+let result = 0;
+
+// CREAZIONE CICLO
+for(let i = 1; i <= 100; i++){
+    
+    // CONDIZIONI PER LA VISUALIZZAZIONE
+    if (i % 15 == 0) {
+        let result = "FizzBuzz";
+        console.log(result)
+    } else if (i % 3 == 0) {
+        let result = "Fizz";
+        console.log(result)
+    } else if (i % 5 == 0) {
+        let result = "Buzz";
+        console.log(result)
+    } else {
+        let result = i;
+        console.log(result)
+    }
+    
+}
