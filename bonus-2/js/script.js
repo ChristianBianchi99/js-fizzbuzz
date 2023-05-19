fff0b3cc5c44c050e66827531e0108d1deb8a788
ChristianBianchi99 <bianchi848@gmail.com>
2023-05-19 19:32:12 +0200

    Agg style - Concluso bonus 2

--- bonus-2/js/script.js
+++ bonus-2/js/script.js
@@ -11,19 +11,19 @@ for(let i = 1; i <= 100; i++){
     if (i % 15 == 0) {
         let result = "FizzBuzz";
         console.log(result)
-        myRow.innerHTML+= `<div class="col text-center purple"><h2>${result}</h2></div>`;
+        myRow.innerHTML+= `<div class="col purple"><h2>${result}</h2></div>`;
     } else if (i % 3 == 0) {
         let result = "Fizz";
         console.log(result)
-        myRow.innerHTML+= `<div class="col text-center red"><h2>${result}</h2></div>`;
+        myRow.innerHTML+= `<div class="col red"><h2>${result}</h2></div>`;
     } else if (i % 5 == 0) {
         let result = "Buzz";
         console.log(result)
-        myRow.innerHTML+= `<div class="col text-center green"><h2>${result}</h2></div>`;
+        myRow.innerHTML+= `<div class="col green"><h2>${result}</h2></div>`;
     } else {
         let result = i;
         console.log(result)
-        myRow.innerHTML+= `<div class="col text-center blue"><h2>${result}</h2></div>`;
+        myRow.innerHTML+= `<div class="col blue"><h2>${result}</h2></div>`;
     }
 
 }
